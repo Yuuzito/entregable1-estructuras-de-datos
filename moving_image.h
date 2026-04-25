cfbae4597935dfed4f8708bb258052249e00f090
Yuuzito <alfonsogg111@gmail.com>
2026-04-24 23:28:27 -0400

    Se hacen funciones undo y redo, incluye test

--- moving_image.h
+++ moving_image.h
@@ -2,6 +2,7 @@
 #define MOVING_IMG_H
 
 #include "basics.h"
+#include <stack>
 
 // Clase que representa una imagen como una colección de 3 matrices siguiendo el
 // esquema de colores RGB
@@ -11,7 +12,8 @@ private:
   unsigned char **red_layer; // Capa de tonalidades rojas
   unsigned char **green_layer; // Capa de tonalidades verdes
   unsigned char **blue_layer; // Capa de tonalidades azules
-
+  std::stack<int> stack_undo; // Pila para almacenar las operaciones realizadas sobre la imagen, con el fin de poder deshacerlas
+  std::stack<int> stack_redo; // Pila para almacenar las operaciones deshechas sobre la imagen, con el fin de poder rehacerlas
 public:
   // Constructor de la imagen. Se crea una imagen por defecto
   moving_image() {
@@ -67,13 +69,19 @@ public:
     _draw(nb);
   }
 
-  // Función que similar desplazar la imagen, de manera circular, d pixeles a la izquierda
   void move_left(int d) {
+    while(!stack_redo.empty()) stack_redo.pop();
+    stack_undo.push(1);
+    stack_undo.push(d);
+    _move_left_internal(d);
+  }
+
+private:
+  void _move_left_internal(int d) {
     unsigned char **tmp_layer = new unsigned char*[H_IMG];
     for(int i=0; i < H_IMG; i++) 
       tmp_layer[i] = new unsigned char[W_IMG];
     
-    // Mover la capa roja
     for(int i=0; i < H_IMG; i++)
       for(int j=0; j < W_IMG-d; j++)
 	      tmp_layer[i][j] = red_layer[i][j+d];      
@@ -86,7 +94,6 @@ public:
       for(int j=0; j < W_IMG; j++)
 	      red_layer[i][j] = tmp_layer[i][j];
 
-    // Mover la capa verde
     for(int i=0; i < H_IMG; i++)
       for(int j=0; j < W_IMG-d; j++)
     	  tmp_layer[i][j] = green_layer[i][j+d];      
@@ -99,7 +106,6 @@ public:
       for(int j=0; j < W_IMG; j++)
     	  green_layer[i][j] = tmp_layer[i][j];
 
-    // Mover la capa azul
     for(int i=0; i < H_IMG; i++)
       for(int j=0; j < W_IMG-d; j++)
     	  tmp_layer[i][j] = blue_layer[i][j+d];      
@@ -111,14 +117,25 @@ public:
     for(int i=0; i < H_IMG; i++)
       for(int j=0; j < W_IMG; j++)
     	  blue_layer[i][j] = tmp_layer[i][j];
+
+    for(int i=0; i < H_IMG; i++) delete[] tmp_layer[i];
+    delete[] tmp_layer;
   }
- 
+
+public:
   void move_right(int d) {
+    while(!stack_redo.empty()) stack_redo.pop();
+    stack_undo.push(2);
+    stack_undo.push(d);
+    _move_right_internal(d);
+  }
+
+private:
+  void _move_right_internal(int d) {
     unsigned char **tmp_layer = new unsigned char*[H_IMG];
     for(int i=0; i < H_IMG; i++) 
       tmp_layer[i] = new unsigned char[W_IMG];
     
-    // Mover la capa roja
     for(int i=0; i < H_IMG; i++)
       for(int j=0; j < W_IMG-d; j++)
 	      tmp_layer[i][j+d] = red_layer[i][j];      
@@ -131,7 +148,6 @@ public:
       for(int j=0; j < W_IMG; j++)
 	      red_layer[i][j] = tmp_layer[i][j];
 
-    // Mover la capa verde
     for(int i=0; i < H_IMG; i++)
       for(int j=0; j < W_IMG-d; j++)
     	  tmp_layer[i][j+d] = green_layer[i][j];      
@@ -144,7 +160,6 @@ public:
       for(int j=0; j < W_IMG; j++)
     	  green_layer[i][j] = tmp_layer[i][j];
 
-    // Mover la capa azul
     for(int i=0; i < H_IMG; i++)
       for(int j=0; j < W_IMG-d; j++)
     	  tmp_layer[i][j+d] = blue_layer[i][j];      
@@ -156,14 +171,26 @@ public:
     for(int i=0; i < H_IMG; i++)
       for(int j=0; j < W_IMG; j++)
     	  blue_layer[i][j] = tmp_layer[i][j];
+
+    for(int i=0; i < H_IMG; i++) delete[] tmp_layer[i];
+    delete[] tmp_layer;
   }
 
+public:
+
   void move_down(int d) {
+    while(!stack_redo.empty()) stack_redo.pop();
+    stack_undo.push(3);
+    stack_undo.push(d);
+    _move_down_internal(d);
+  }
+
+private:
+  void _move_down_internal(int d) {
     unsigned char **tmp_layer = new unsigned char*[H_IMG];
     for(int i=0; i < H_IMG; i++) 
       tmp_layer[i] = new unsigned char[W_IMG];
     
-    // Mover la capa roja
     for(int i = 0; i < H_IMG - d; i++)
       for(int j = 0; j < W_IMG; j++)
         tmp_layer[i+d][j] = red_layer[i][j];
@@ -176,7 +203,6 @@ public:
       for(int j=0; j < W_IMG; j++)
         red_layer[i][j] = tmp_layer[i][j];
 
-    // Mover la capa verde
     for(int i = 0; i < H_IMG - d; i++)
       for(int j = 0; j < W_IMG; j++)
         tmp_layer[i+d][j] = green_layer[i][j];
@@ -189,8 +215,6 @@ public:
       for(int j=0; j < W_IMG; j++)
         green_layer[i][j] = tmp_layer[i][j];
 
-    // Mover la capa azul
-
     for(int i = 0; i < H_IMG - d; i++)
       for(int j = 0; j < W_IMG; j++)
         tmp_layer[i+d][j] = blue_layer[i][j];
@@ -202,14 +226,26 @@ public:
     for(int i=0; i < H_IMG; i++)
       for(int j=0; j < W_IMG; j++)
         blue_layer[i][j] = tmp_layer[i][j];
+
+    for(int i=0; i < H_IMG; i++) delete[] tmp_layer[i];
+    delete[] tmp_layer;
   }
 
+public:
+
   void move_up(int d) {
+    while(!stack_redo.empty()) stack_redo.pop();
+    stack_undo.push(4);
+    stack_undo.push(d);
+    _move_up_internal(d);
+  }
+
+private:
+  void _move_up_internal(int d) {
     unsigned char **tmp_layer = new unsigned char*[H_IMG];
     for(int i=0; i < H_IMG; i++) 
       tmp_layer[i] = new unsigned char[W_IMG];
     
-    // Mover la capa roja
     for(int j=0; j < W_IMG; j++)
       for(int i=d; i < H_IMG; i++)
 	      tmp_layer[i-d][j] = red_layer[i][j];      
@@ -222,7 +258,6 @@ public:
       for(int j=0; j < W_IMG; j++)
 	      red_layer[i][j] = tmp_layer[i][j];
 
-    // Mover la capa verde
     for(int j=0; j < W_IMG; j++)
       for(int i=d; i < H_IMG; i++)
 	      tmp_layer[i-d][j] = green_layer[i][j];      
@@ -235,7 +270,6 @@ public:
       for(int j=0; j < W_IMG; j++)
     	  green_layer[i][j] = tmp_layer[i][j];
 
-    // Mover la capa azul
     for(int j=0; j < W_IMG; j++)
       for(int i=d; i < H_IMG; i++)
 	      tmp_layer[i-d][j] = blue_layer[i][j];      
@@ -247,41 +281,125 @@ public:
     for(int i=0; i < H_IMG; i++)
       for(int j=0; j < W_IMG; j++)
     	  blue_layer[i][j] = tmp_layer[i][j];
-  }
-  void rotate() {
-  unsigned char **tmp_layer = new unsigned char*[H_IMG];
-  for(int i=0; i < H_IMG; i++) 
-    tmp_layer[i] = new unsigned char[W_IMG];
 
-  // Mover capa roja
-  for(int i=0; i < H_IMG; i++)
-    for(int j=0; j < W_IMG; j++)
-      tmp_layer[H_IMG - j - 1][i] = red_layer[i][j];
-
-  for(int i=0; i < H_IMG; i++)
-    for(int j=0; j < W_IMG; j++)
-      red_layer[i][j] = tmp_layer[i][j];
-
-  // Mover capa verde
-  for(int i=0; i < H_IMG; i++)
-    for(int j=0; j < W_IMG; j++)
-      tmp_layer[H_IMG - j - 1][i] = green_layer[i][j];
-
-  for(int i=0; i < H_IMG; i++)
-    for(int j=0; j < W_IMG; j++)
-      green_layer[i][j] = tmp_layer[i][j];
-
-  // Mover capa azul
-  for(int i=0; i < H_IMG; i++)
-    for(int j=0; j < W_IMG; j++)
-      tmp_layer[H_IMG - j - 1][i] = blue_layer[i][j];
-
-  for(int i=0; i < H_IMG; i++)
-    for(int j=0; j < W_IMG; j++)
-      blue_layer[i][j] = tmp_layer[i][j];
-
-  for(int i=0; i < H_IMG; i++) delete[] tmp_layer[i];
+    for(int i=0; i < H_IMG; i++) delete[] tmp_layer[i];
     delete[] tmp_layer;
+  }
+
+public:
+  void rotate() {
+    while(!stack_redo.empty()) stack_redo.pop();
+    stack_undo.push(5);
+    stack_undo.push(90);
+    _rotate_internal();
+  }
+
+private:
+  void _rotate_internal() {
+    unsigned char **tmp_layer = new unsigned char*[H_IMG];
+    for(int i=0; i < H_IMG; i++) 
+      tmp_layer[i] = new unsigned char[W_IMG];
+
+    for(int i=0; i < H_IMG; i++)
+      for(int j=0; j < W_IMG; j++)
+        tmp_layer[H_IMG - j - 1][i] = red_layer[i][j];
+
+    for(int i=0; i < H_IMG; i++)
+      for(int j=0; j < W_IMG; j++)
+        red_layer[i][j] = tmp_layer[i][j];
+
+    for(int i=0; i < H_IMG; i++)
+      for(int j=0; j < W_IMG; j++)
+        tmp_layer[H_IMG - j - 1][i] = green_layer[i][j];
+
+    for(int i=0; i < H_IMG; i++)
+      for(int j=0; j < W_IMG; j++)
+        green_layer[i][j] = tmp_layer[i][j];
+
+    for(int i=0; i < H_IMG; i++)
+      for(int j=0; j < W_IMG; j++)
+        tmp_layer[H_IMG - j - 1][i] = blue_layer[i][j];
+
+    for(int i=0; i < H_IMG; i++)
+      for(int j=0; j < W_IMG; j++)
+        blue_layer[i][j] = tmp_layer[i][j];
+
+    for(int i=0; i < H_IMG; i++) delete[] tmp_layer[i];
+    delete[] tmp_layer;
+  }
+
+public:
+
+void undo() {
+  if(stack_undo.empty()) {
+    printf("No hay operaciones para deshacer\n");
+    return;
+  }
+
+  int d = stack_undo.top();
+  stack_undo.pop();
+  int op = stack_undo.top();
+  stack_undo.pop();
+
+  stack_redo.push(op);
+  stack_redo.push(d);
+
+  switch(op) {
+    case 1:
+      _move_right_internal(d);
+      break;
+    case 2:
+      _move_left_internal(d);
+      break;
+    case 3:
+      _move_up_internal(d);
+      break;
+    case 4:
+      _move_down_internal(d);
+      break;
+    case 5:
+      _rotate_internal();
+      _rotate_internal();
+      _rotate_internal();
+      break;
+    default:
+      break;
+  }
+}
+
+void redo() {
+  if(stack_redo.empty()) {
+    printf("No hay operaciones para rehacer\n");
+    return;
+  }
+
+  int d = stack_redo.top();
+  stack_redo.pop();
+  int op = stack_redo.top();
+  stack_redo.pop();
+
+  stack_undo.push(op);
+  stack_undo.push(d);
+
+  switch(op) {
+    case 1:
+      _move_left_internal(d);
+      break;
+    case 2:
+      _move_right_internal(d);
+      break;
+    case 3:
+      _move_down_internal(d);
+      break;
+    case 4:
+      _move_up_internal(d);
+      break;
+    case 5:
+      _rotate_internal();
+      break;
+    default:
+      break;
+  }
 }
 
 private:
@@ -305,6 +423,7 @@ private:
     // La función svpng() transforma las 3 matrices RGB en una imagen PNG 
     svpng(fp, W_IMG, H_IMG, rgb, 0);
     fclose(fp);
+    delete[] rgb;
 }
 
   
--- test.cpp
+++ test.cpp
@@ -6,25 +6,63 @@
  */
 
 #include "moving_image.h"
-#include<unistd.h> // para sleep (linux). Usar  #include<windows.h> para Windows
+#include<windows.h> // para sleep (linux). Usar  #include<windows.h> para Windows
+#include<iostream>
 
 int main() {
-  moving_image im;
+  std::cout << "--- INICIANDO TEST DE MOVING IMAGE ---\n\n";
 
-  im.draw("imagen.png");
-  im.move_left(500);
-  sleep(2);
-  im.draw("imagen_desplazada_500_left.png");
-  im.move_right(100);
-  sleep(2);
-  im.draw("imagen_desplazada_100_right.png");
-  im.move_down(100);
-  sleep(2);
-  im.draw("imagen_desplazada_100_down.png");
-  im.move_up(100);
-  sleep(2);
-  im.draw("imagen_desplazada_100_up.png");
+  // 1. Instanciamos la imagen (esto llama al constructor)
+  moving_image mi_imagen;
+  std::cout << "Imagen creada. Guardando estado inicial...\n";
+  mi_imagen.draw("paso_0_inicial.png");
 
+  // 2. Hacemos algunos movimientos
+  std::cout << "\n--- HACIENDO MOVIMIENTOS ---\n";
+    
+  std::cout << "Moviendo a la derecha 50 pixeles...\n";
+  mi_imagen.move_right(50);
+  mi_imagen.draw("paso_1_derecha.png");
+
+  std::cout << "Moviendo hacia abajo 80 pixeles...\n";
+  mi_imagen.move_down(80);
+  mi_imagen.draw("paso_2_abajo.png");
+
+  std::cout << "Rotando 90 grados...\n";
+  mi_imagen.rotate();
+  mi_imagen.draw("paso_3_rotado.png");
+
+  // 3. Probamos el UNDO (Deshacer)
+  std::cout << "\n--- PROBANDO UNDO (Viaje al pasado) ---\n";
+    
+  mi_imagen.undo(); // Debería deshacer la rotación
+  mi_imagen.draw("paso_4_undo_rotacion.png");
+
+  mi_imagen.undo(); // Debería deshacer el movimiento hacia abajo
+  mi_imagen.draw("paso_5_undo_abajo.png");
+
+  // 4. Probamos el REDO (Viaje al futuro)
+  std::cout << "\n--- PROBANDO REDO (Viaje al futuro) ---\n";
+    
+  mi_imagen.redo(); // Debería rehacer el movimiento hacia abajo
+  mi_imagen.draw("paso_6_redo_abajo.png");
+
+  // 5. Probamos la bifurcación temporal (hacer algo nuevo tras un undo)
+  std::cout << "\n--- PROBANDO NUEVA LINEA TEMPORAL ---\n";
+    
+  mi_imagen.undo(); // Volvemos a quitar el movimiento hacia abajo
+  mi_imagen.draw("paso_7_undo_abajo.png");
+  std::cout << "Se deshizo el movimiento hacia abajo.\n";
+    
+  std::cout << "Moviendo a la izquierda 30 pixeles (Crea nueva linea temporal)...\n";
+  mi_imagen.move_left(30);
+  mi_imagen.draw("paso_8_nueva_linea_izq.png");
+
+  std::cout << "Intentando rehacer (Deberia fallar porque el futuro fue borrado)...\n";
+  mi_imagen.redo(); // Aquí tu programa debería imprimir "No hay operaciones para rehacer"
+
+  std::cout << "\n--- TEST FINALIZADO CORRECTAMENTE ---\n";
+  std::cout << "Revisa tu carpeta para ver los archivos .png generados.\n";
   /* NOTA 1: Si usan el mismo nombre para las imágenes, entonces cada llamada al
   método draw() sobreescribirá a la imagen */
 
